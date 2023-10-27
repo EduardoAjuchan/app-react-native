c2c24d7f8b651fd1e816f3d9ee50eaef60477c38
EduardoAjuchan <eduardoajuchan@gmail.com>
2023-10-26 22:44:04 -0600

    First commit and routes screens

---
+++ screens/DetailsNote.js
@@ -0,0 +1,14 @@
+import React,{useState, useEffect} from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+
+export default function DetailsNote (){
+  
+    return (
+      <View>
+        <Text>Notas</Text>
+      </View>
+    )
+  }
+
+const styles = StyleSheet.create({})
---
+++ screens/Notas.js
@@ -0,0 +1,14 @@
+import React,{useState, useEffect} from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+
+export default function Notas (){
+  
+    return (
+      <View>
+        <Text>Notas</Text>
+      </View>
+    )
+  }
+
+const styles = StyleSheet.create({})
