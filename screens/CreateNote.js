041d1aaee76e154790842d18c9c6470dfb4c0ccc
EduardoAjuchan <eduardoajuchan@gmail.com>
2023-10-27 19:09:06 -0600

    "Confirm and Cancel Buttons on DTP"

--- screens/CreateNote.js
+++ screens/CreateNote.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Text, StyleSheet, View, TextInput, TouchableOpacity, Platform } from 'react-native';
+import { Text, StyleSheet, View, TextInput, TouchableOpacity, Platform, ScrollView, Pressable } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function CreateNote(props) {
@@ -15,29 +15,25 @@ export default function CreateNote(props) {
   const [fecha, setFecha] = useState('');
   const [hora, setHora] = useState('');
   const [estado, setEstado] = useState(initialState);
-  const [datePickerVisible, setDatePickerVisible] = useState(false);
 
   const onChange = (event, selectedDate) => {
-    if (datePickerVisible) {
-      const currentDate = selectedDate || date;
-      setShow(Platform.OS === 'ios');
-      setDate(currentDate);
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
 
-      let tempDate = new Date(currentDate);
-      if (mode === 'date') {
-        let fDate =
-          tempDate.getDate() +
-          '/' +
-          (tempDate.getMonth() + 1) +
-          '/' +
-          tempDate.getFullYear();
-        setFecha(fDate);
-      } else if (mode === 'time') {
-        let fTime =
-          'Hora: ' + tempDate.getHours() + ':' + 'Minutos: ' + tempDate.getMinutes();
-        setHora(fTime);
-      }
-      setDatePickerVisible(false);
+    let tempDate = new Date(currentDate);
+    if (mode === 'date') {
+      let fDate =
+        tempDate.getDate() +
+        '/' +
+        (tempDate.getMonth() + 1) +
+        '/' +
+        tempDate.getFullYear();
+      setFecha(fDate);
+    } else if (mode === 'time') {
+      let fTime =
+        'Hora: ' + tempDate.getHours() + ':' + 'Minutos: ' + tempDate.getMinutes();
+      setHora(fTime);
     }
   };
 
@@ -47,74 +43,110 @@ export default function CreateNote(props) {
   };
 
   const hideDateTimePicker = () => {
-    setDatePickerVisible(false);
+    setShow(false);
+  };
+
+ //boton cancelar debe limpiar el input de fecha y hora
+  const cancelDateTimePicker = () => {
+    setFecha('');
+    setHora('');
+    setShow(false);
+  };
+
+  const saveDateTimePicker = () => {
+    setShow(false);
   };
 
   return (
-    <View style={styles.contenedorPadre}>
-      <View style={styles.tarjeta}>
-        <View style={styles.contenedor}>
-          <TextInput
-            placeholder="Ingresa el titulo"
-            style={styles.textoinput}
-          />
-          <TextInput
-            placeholder="Ingresa la descripción"
-            multiline={true}
-            numberOfLines={4}
-            style={styles.textoinput}
-          />
-          {/* Contenedor de la fecha */}
-          <View style={styles.inputDate}>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.contenedorPadre}>
+        <View style={styles.tarjeta}>
+          <View style={styles.contenedor}>
             <TextInput
-              placeholder="Ingresa la fecha dd/mm/aa"
-              style={styles.textoDate}
-              value={fecha}
+              placeholder="Ingresa el titulo"
+              style={styles.textoinput}
             />
-            <TouchableOpacity
-              style={styles.botonDate}
+            <TextInput
+              placeholder="Ingresa la descripción"
+              multiline={true}
+              numberOfLines={4}
+              style={styles.textoinput}
+            />
+            {/* Contenedor de la fecha */}
+            
+              <Pressable
               onPress={() => showDateTimePicker('date')}
-            >
-              <Text style={styles.subtitle}>Fecha</Text>
-            </TouchableOpacity>
-          </View>
-          {/* Contenedor de hora */}
-          <View style={styles.inputDate}>
-            <TextInput
-              placeholder="Hora hh/mm"
-              style={styles.textoDate}
-              value={hora}
-            />
-            <TouchableOpacity
-              style={styles.botonDate}
+              >
+      
+              <TextInput
+              onPressIn={() => showDateTimePicker('date')}
+                placeholder="Ingresa la fecha dd/mm/aa"
+                style={styles.textoDate}
+                value={fecha}
+                editable={false}
+  
+              />
+              </Pressable>
+            
+            {/* Contenedor de hora */}
+           <Pressable
               onPress={() => showDateTimePicker('time')}
-            >
-              <Text style={styles.subtitle}>Hora</Text>
-            </TouchableOpacity>
+              >
+              <TextInput
+                onPressIn={() => showDateTimePicker('time')}
+                placeholder="Hora hh/mm"
+                style={styles.textoDate}
+                value={hora}
+                editable={false}
+                
+              />
+              </Pressable>
+
+            {show && (
+              <DateTimePicker
+                testID="dateTimePicker"
+                value={date}
+                mode={mode}
+                is24Hour={true}
+                display="spinner"
+                onChange={onChange}
+                minimumDate={new Date('2023-01-01')}
+              />
+            )}
+            {show && Platform.OS === 'ios' && (
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                  style={[styles.button, styles.pickerButton, { backgroundColor: '#11180711' }]
+                }
+                onPress={cancelDateTimePicker}
+                >
+                  <Text style={[styles.buttonText, { color: '#075985' }]}>Cancelar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={[styles.button, styles.pickerButton, { backgroundColor: '#11180711' }]
+                }
+                onPress={saveDateTimePicker}
+                >
+                  <Text style={[styles.buttonText, { color: '#075985' }]}>Aceptar</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+            <View>
+              <TouchableOpacity style={styles.button}>
+                <Text style={styles.textoBtnEnviar}>Guardar</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-          <View>
-            <TouchableOpacity style={styles.botonEnviar}>
-              <Text style={styles.textoBtnEnviar}>Guardar</Text>
-            </TouchableOpacity>
-          </View>
-          {show && (
-            <DateTimePicker
-              testID="dateTimePicker"
-              value={date}
-              mode={mode}
-              is24Hour={true}
-              display="spinner"
-              onChange={onChange}
-              minimumDate={new Date('2023-01-01')}
-            />
-          )}
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+  },
   contenedorPadre: {
     flex: 1,
     justifyContent: 'center',
@@ -146,26 +178,11 @@ const styles = StyleSheet.create({
     width: '100%',
     flexWrap: 'wrap',
     flexDirection: 'row',
-  },
-  botonDate: {
-    backgroundColor: '#006BA6',
-    borderRadius: 10,
-    justifyContent: 'center',
     alignItems: 'center',
-    marginLeft: 110,
-    padding: 10,
   },
-  textoDate: {
-    borderBottomColor: '#ccc',
-    borderBottomWidth: 1,
-    padding: 2,
-    marginBottom: 30,
-    marginTop: 20,
-    width: '100%',
-  },
-  subtitle: {
-    color: 'white',
-    fontSize: 16,
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
   botonEnviar: {
     backgroundColor: '#00c6ab',
@@ -178,6 +195,35 @@ const styles = StyleSheet.create({
   textoBtnEnviar: {
     textAlign: 'center',
     padding: 10,
+    color: 'white',
+    fontSize: 16,
+  },
+  pickerButton: {
+    paddingHorizontal: 20,
+  },
+  button: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 50,
+    marginTop: 10,
+    marginBottom: 15,
+    backgroundColor: '#006BA6',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: '500',
+    fontSize: 14,
+  },
+  textoDate: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    padding: 2,
+    marginBottom: 30,
+    marginTop: 20,
+    width: '100%',
+  },
+  subtitle: {
     color: 'white',
     fontSize: 16,
   },
