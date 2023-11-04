b73177123fff0e1c05b125ccc4a2006608abe73f
EduardoAjuchan <eduardoajuchan@gmail.com>
2023-11-03 21:50:29 -0600

    Edit assets and complete apk

--- credenciales.js
+++ credenciales.js
@@ -1,18 +1,1 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCovaz5v1PM1APvAZ9GNqZdzsupNlWRm6k",
-  authDomain: "appnotas-4fc83.firebaseapp.com",
-  projectId: "appnotas-4fc83",
-  storageBucket: "appnotas-4fc83.appspot.com",
-  messagingSenderId: "808013053667",
-  appId: "1:808013053667:web:8cd112e7fec4c789ad8fd6"
-};
-
-// Initialize Firebase
-const appFirebase = initializeApp(firebaseConfig);
-export default appFirebase;
+//aqui pones tus credenciales de Firebase
